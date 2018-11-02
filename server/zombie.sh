0105c600b620d6bedd4ed1cd53d9c60724f93cdb
chuyj <roger9chu@gmail.com>
2018-11-02 17:03:42 +0800

    zombie fit FreeBSD and Linux

--- server/zombie.sh
+++ server/zombie.sh
@@ -1,9 +1,14 @@
 #! /bin/sh
 # Test zombie process before you shut down your server
 
+OS=$( uname -s )
 USER=$( whoami )
 
-if [ -z "$( ps -u ${USER} -U ${USER} -o state | grep Z )" ]; then
+[ ${OS} = "FreeBSD" ] && PS_COMMAND="ps -o state"
+[ ${OS} = "Linux" ] && PS_COMMAND="ps -u ${USER} -U ${USER} -o state"
+
+
+if [ -z "$( ${PS_COMMAND} | grep Z )" ]; then
   echo "[0;32mGood Job![m"
 else
   echo "[0;31mYou left zombie processes in the system![m"
