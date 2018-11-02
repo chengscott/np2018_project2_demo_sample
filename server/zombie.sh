64f58fbee3de9d84ef35b3a74110a1141aaf8fde
chuyj <roger9chu@gmail.com>
2018-11-02 16:55:08 +0800

    zombie and shared memory test update

--- server/zombie.sh
+++ server/zombie.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 # Test zombie process before you shut down your server
-if [ -z "$( ps -o state | grep Z )" ]; then
+
+USER=$( whoami )
+
+if [ -z "$( ps -u ${USER} -U ${USER} -o state | grep Z )" ]; then
   echo "[0;32mGood Job![m"
 else
   echo "[0;31mYou left zombie processes in the system![m"
