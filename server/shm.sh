86c0e019e25384499f9bfddaed89d1c97930482a
chuyj <roger9chu@gmail.com>
2018-11-13 21:55:08 +0800

    shm.sh -mc

--- server/shm.sh
+++ server/shm.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 # Test shared memory after you shut down your server
-if [ -z "$( ipcs -m | grep $( whoami ) )" ]; then
+if [ -z "$( ipcs -mc | grep $( whoami ) )" ]; then
   echo "[0;32mGood Job![m"
 else
   echo "[0;31mYou left shared memory in the system![m"
